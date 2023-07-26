ccf0f602234023c1c219b69bba03bfba3720d512
Yvan Epale <epaleyvan@gmail.com>
2023-07-26 15:03:41 +0100

    Finalisation of the projects

--- webapp/src/main/java/com/eyangbeauty/webapp/serviceImpl/EmailServiceImpl.java
+++ webapp/src/main/java/com/eyangbeauty/webapp/serviceImpl/EmailServiceImpl.java
@@ -23,7 +23,7 @@ public class EmailServiceImpl implements IEmail {
         String newLine = System.getProperty("line.separator");
         String text = "M/Mme/Mlle " + customerDto.getFullName() + newLine +
                 "Ce message vous a été envoyé pour confirmer l'enregistrement de votre réservation à Eyang Beauty & Spa. " + newLine +
-                "En effet, vous avez pris rendez-vous le " + formReservation.getDate() + " à " + formReservation.getTime() + "Dans notre institut. " + newLine +
+                "En effet, vous avez pris rendez-vous le " + formReservation.getDate() + " à " + formReservation.getTime() + " dans notre institut. " + newLine +
                 "N'oubliez pas votre rendez-vous. " + newLine +
                 "Eyang Beauty and Spa, au plaisir de vous servir.";
 
